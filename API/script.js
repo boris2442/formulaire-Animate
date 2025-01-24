1c714a6ec60dc68096a7e1ab4a9a23c8e25fa694
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-24 10:34:45 +0100

    text

---
+++ API/script.js
@@ -0,0 +1,27 @@
+// la methode XMLHttpRequest
+// function reqListener() {
+//   // console.log(this.responseText);
+// }
+
+// let req= new XMLHttpRequest();
+
+// req.onload = reqListener;
+// req.open('get', 'data.json', true);
+// req.open('get', 'data.txt', true)
+// req.send();
+
+// fetch("monlien", "object d\'option").then((response)=>{
+//     response
+// })
+// .catch((err)=>console.log(err))
+
+// fetch("data.txt").then((resp)=>console.log(resp));
+
+// fetch("data.txt")
+//   .then((res) => res.text())
+//   .then((data) => console.log(data));
+// fetch('data.json').then((res) =>console.log(res));
+
+// fetch("data.json")
+// .then((res) =>res.json())
+// .then((data) =>console.log(data))
